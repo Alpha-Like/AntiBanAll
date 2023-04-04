38b6279fb571142e6ed23522d770fa9be16cb9d4
Keshava Tripathi <124826956+ShutupKeshav@users.noreply.github.com>
2023-04-04 20:38:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+from os import getenv as g
+
+API_ID = int(g("API_ID", None))
+API_HASH = g("API_HASH", "")
+
+BOT_TOKEN = g("BOT_TOKEN", "")
+
+GROUP_IDS = g("GROUP_IDS", "").split() # example : "-123 -566"
+ADMIN_IDS = g("ADMIN_IDS", "").split() # example : "12578627 74992683"
+
+# DONT EDIT CODE BELOW !
+
+ADMINS = []
+GROUPS = []
+
+for x in ADMIN_IDS:
+    ADMINS.append(int(x))
+
+for y in GROUP_IDS:
+    GROUPS.append(int(y))
